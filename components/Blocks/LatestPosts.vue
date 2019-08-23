5fe9ed6702fb70579bd27950369a992a7ec0470d
Michiel Koning <mail@michielkoning.nl>
2019-08-23 07:08:02 +0200

    balloon

--- components/Blocks/LatestPosts.vue
+++ components/Blocks/LatestPosts.vue
@@ -10,27 +10,24 @@
       <a href="#" class="btn">Meer nieuwsartikelen</a>
     </div>
 
-    <div class="balloon">
-      <img src="/images/air-balloon.png" alt="" class="balloon-image" />
-    </div>
+    <balloon />
   </section>
 </template>
 
 <script>
 //  import axios from 'axios'
-import { mapActions, mapState, mapGetters } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 import AppPost from '@/components/AppPost.vue'
+import Balloon from '@/components/Illustrations/Balloon.vue'
 
 export default {
   components: {
-    AppPost
+    AppPost,
+    Balloon
   },
 
   computed: {
-    ...mapState('posts', ['list', 'isLoading', 'currentPage', 'totalPages']),
-    ...mapGetters({
-      hasAllPostsLoaded: 'posts/hasAllPostsLoaded'
-    })
+    ...mapState('posts', ['list'])
   },
 
   mounted() {
@@ -59,33 +56,5 @@ export default {
 .list {
   @mixin list-reset;
   max-width: var(--container-width-md);
-}
-
-.balloon {
-  position: absolute;
-  bottom: 4rem;
-  right: 4rem;
-}
-
-.balloon {
-  animation: balloonX 20s infinite cubic-bezier(0.02, 0.01, 0.21, 1);
-}
-
-.balloon-image {
-  animation: balloonY 20s infinite cubic-bezier(0.3, 0.27, 0.07, 1.64);
-}
-
-@keyframes balloonY {
-  50% {
-    animation-timing-function: cubic-bezier(0.02, 0.01, 0.21, 1);
-    transform: translateY(-8rem);
-  }
-}
-
-@keyframes balloonX {
-  50% {
-    animation-timing-function: cubic-bezier(0.3, 0.27, 0.07, 1.64);
-    transform: translateX(-2rem);
-  }
 }
 </style>
---
+++ components/Illustrations/Balloon.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="balloon">
+    <img src="/images/air-balloon.png" alt="" class="balloon-image" />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.balloon {
+  position: absolute;
+  bottom: 4rem;
+  right: 4rem;
+}
+
+.balloon {
+  animation: balloonX 20s infinite cubic-bezier(0.02, 0.01, 0.21, 1);
+}
+
+.balloon-image {
+  animation: balloonY 20s infinite cubic-bezier(0.3, 0.27, 0.07, 1.64);
+}
+
+@keyframes balloonY {
+  50% {
+    animation-timing-function: cubic-bezier(0.02, 0.01, 0.21, 1);
+    transform: translateY(-8rem);
+  }
+}
+
+@keyframes balloonX {
+  50% {
+    animation-timing-function: cubic-bezier(0.3, 0.27, 0.07, 1.64);
+    transform: translateX(-2rem);
+  }
+}
+</style>
