96a03e9e4e0784acac7e35c68e50d1892801ef9d
Michiel Koning <mail@michielkoning.nl>
2019-08-24 21:24:32 +0200

    animate menu

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -1,123 +1,140 @@
 <template>
   <nav aria-labelledby="menu-title">
     <h2 id="menu-title" class="sr-only">{{ $t('mainNavigation') }}</h2>
-    <ul ref="menu" class="list">
-      <li>
-        <nuxt-link to="/">Home</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/posters/">Posters</nuxt-link>
-      </li>
-      <li
-        :class="{ open: isOpen }"
-        class="has-popup"
-        @mouseover="mouseover"
-        @mouseout="mouseout"
-      >
-        <nuxt-link
-          to="/informatie/"
-          :aria-expanded="isOpen"
-          aria-haspopup="true"
-          >Over mij</nuxt-link
+    <div class="wrapper">
+      <ul ref="menu" class="list">
+        <li>
+          <nuxt-link to="/"><span class="title">Home</span></nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/posters/"
+            ><span class="title">Posters</span></nuxt-link
+          >
+        </li>
+        <li
+          :class="{ open: isOpen }"
+          class="has-popup"
+          @mouseover="mouseover"
+          @mouseout="mouseout"
         >
-        <button
-          class="btn-show-submenu"
-          :aria-expanded="isOpen"
-          @click="toggleMenu"
-        >
-          <icon-chevron-down
-            aria-hidden="true"
-            width="20"
-            height="20"
-            class="icon-chevron-down"
-          />
-          <span class="sr-only">Toon submenu voor Over mij</span>
-        </button>
-        <ul class="submenu">
-          <li>
-            <nuxt-link to="/informatie/wat-wil-loesje/"
-              >Wie is Loesje</nuxt-link
-            >
-          </li>
-          <li>
-            <nuxt-link to="/informatie/internationaal/"
-              >Loesje in de wijde wereld</nuxt-link
-            >
-          </li>
-          <li>
-            <nuxt-link to="/informatie/nieuws/">Nieuws</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/informatie/copyright/">Copyright</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/informatie/contact-2/">Contact</nuxt-link>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <nuxt-link to="/doe-mee/">Doe mee</nuxt-link>
-        <ul>
-          <li>
-            <nuxt-link to="/doe-mee/online-meeschrijven/"
-              >Schrijf online mee</nuxt-link
-            >
-          </li>
-          <li>
-            <nuxt-link to="/doe-mee/lokale-groepen/"
-              >Schrijf mee in je buurt</nuxt-link
-            >
-          </li>
-          <li>
-            <nuxt-link to="/doe-mee/word-lid/">Word lid</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/doe-mee/word-donateur/">Word donateur</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/doe-mee/agenda-2/">Agenda</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/doe-mee/blijf-op-hoogte/"
-              >Blijf op de hoogte</nuxt-link
-            >
-          </li>
-          <li>
-            <nuxt-link to="/doe-mee/vacatures/">Vacatures</nuxt-link>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <nuxt-link to="/workshops/">Workshop</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/winkeltje/">Winkeltje</nuxt-link>
-        <ul>
-          <li>
-            <nuxt-link to="/winkeltje/categorie/kleding/">Kleding</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/winkeltje/categorie/kalenders/"
-              >Kalenders en agenda&#8217;s</nuxt-link
-            >
-          </li>
-          <li>
-            <nuxt-link to="/winkeltje/categorie/boeken/">Boeken</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/winkeltje/categorie/diversen/">Diversen</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/winkeltje/categorie/kaarten/">Kaarten</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/winkeltje/categorie/aanbieding/"
-              >Aanbieding</nuxt-link
-            >
-          </li>
-        </ul>
-      </li>
-    </ul>
+          <nuxt-link
+            to="/informatie/"
+            :aria-expanded="isOpen"
+            aria-haspopup="true"
+            >Over mij</nuxt-link
+          >
+          <button
+            class="btn-show-submenu"
+            :aria-expanded="isOpen"
+            @click="toggleMenu"
+          >
+            <icon-chevron-down
+              aria-hidden="true"
+              width="20"
+              height="20"
+              class="icon-chevron-down"
+            />
+            <span class="sr-only">Toon submenu voor Over mij</span>
+          </button>
+          <ul class="submenu">
+            <li>
+              <nuxt-link to="/informatie/wat-wil-loesje/"
+                >Wie is Loesje</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/informatie/internationaal/"
+                >Loesje in de wijde wereld</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/informatie/nieuws/">Nieuws</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/informatie/copyright/">Copyright</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/informatie/contact-2/">Contact</nuxt-link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <nuxt-link to="/doe-mee/"
+            ><span class="title">Doe mee</span></nuxt-link
+          >
+          <ul>
+            <li>
+              <nuxt-link to="/doe-mee/online-meeschrijven/"
+                >Schrijf online mee</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/doe-mee/lokale-groepen/"
+                >Schrijf mee in je buurt</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/doe-mee/word-lid/">Word lid</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/doe-mee/word-donateur/">Word donateur</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/doe-mee/agenda-2/">Agenda</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/doe-mee/blijf-op-hoogte/"
+                >Blijf op de hoogte</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/doe-mee/vacatures/">Vacatures</nuxt-link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <nuxt-link to="/workshops/"
+            ><span class="title">Workshop</span></nuxt-link
+          >
+        </li>
+        <li>
+          <nuxt-link to="/winkeltje/"
+            ><span class="title">Winkeltje</span></nuxt-link
+          >
+          <ul>
+            <li>
+              <nuxt-link to="/winkeltje/categorie/kleding/">Kleding</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/winkeltje/categorie/kalenders/"
+                >Kalenders en agenda&#8217;s</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/winkeltje/categorie/boeken/">Boeken</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/winkeltje/categorie/diversen/"
+                >Diversen</nuxt-link
+              >
+            </li>
+            <li>
+              <nuxt-link to="/winkeltje/categorie/kaarten/">Kaarten</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/winkeltje/categorie/aanbieding/"
+                >Aanbieding</nuxt-link
+              >
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div
+        class="arrow"
+        :class="{ active: mounted }"
+        :style="{ transform: arrowPosition, width: arrowWidth }"
+      />
+    </div>
   </nav>
 </template>
 
@@ -131,10 +148,36 @@ export default {
   data() {
     return {
       isOpen: false,
-      timer: null
+      timer: null,
+      arrowPosition: 0,
+      arrowWidth: 0,
+      mounted: false
     }
   },
+
+  watch: {
+    $route() {
+      this.$nextTick(() => {
+        this.setArrowPosition()
+      })
+    }
+  },
+  mounted() {
+    this.setArrowPosition()
+    setTimeout(() => {
+      this.mounted = true
+    }, 0)
+  },
   methods: {
+    setArrowPosition() {
+      const activeLink = this.$refs.menu.querySelector(
+        '.nuxt-link-active[aria-haspopup=true] > .title, .nuxt-link-exact-active > .title'
+      )
+      if (activeLink) {
+        this.arrowPosition = `translate(${activeLink.parentElement.offsetLeft}px, -0.5em)`
+        this.arrowWidth = `${activeLink.offsetWidth}px`
+      }
+    },
     toggleMenu() {
       this.isOpen = !this.isOpen
     },
@@ -162,12 +205,17 @@ nav {
   margin-bottom: 1em;
 }
 
+.wrapper {
+  @mixin center;
+}
+
 .list {
   @mixin list-reset;
-  @mixin center;
+  margin-bottom: 0;
   position: relative;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
+  padding-top: 4px;
 
   & ul {
     display: none;
@@ -188,10 +236,24 @@ a {
   @mixin link-reset;
   display: block;
   padding: 0.25em 0;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus > .title,
+  &:hover > .title {
+    box-shadow: 0 2px 0 0 currentColor;
+  }
+}
+
+.title {
+  transition: box-shadow 0.2s ease-out;
 }
 
 .btn-show-submenu {
   transform: translate(-0.25em, 0.25em);
+  display: none;
 }
 
 .submenu {
@@ -207,6 +269,16 @@ a {
 
   @nest .open & {
     display: block;
+  }
+}
+
+.arrow {
+  height: 4px;
+  background: currentColor;
+  display: block;
+
+  &.active {
+    transition: all 0.2s ease-out;
   }
 }
 </style>
