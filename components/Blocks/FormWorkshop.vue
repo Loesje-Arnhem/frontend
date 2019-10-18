1a2748de6e0876f2e5e20c435cc994d6645ddd57
Michiel Koning <mail@michielkoning.nl>
2019-10-18 22:24:51 +0200

    formworkshop

--- components/Blocks/FormWorkshop.vue
+++ components/Blocks/FormWorkshop.vue
@@ -1,63 +1,147 @@
 <template>
-  <form class="form">
-    <div class="wrapper">
+  <div class="wrapper">
+    <p v-if="submitted">
+      {{ $t('form.forms.contact.success') }}
+    </p>
+
+    <app-form
+      v-else
+      class="form"
+      name="workshop"
+      button-title="Verzenden"
+      @submit="submitForm"
+    >
       <form-fieldset title="Contact">
         <form-input-text
           id="email"
-          v-model="email"
+          v-model="form.email"
           title="E-mailadres"
           type="email"
           name="email"
+          :error-message="errorMessageEmail"
         />
-        <form-input-text id="name" v-model="name" title="Naam" name="name" />
+        <form-input-text
+          id="name"
+          v-model="form.name"
+          title="Naam"
+          name="name"
+          :error-message="errorMessageName"
+        />
         <form-input-text
           id="phoneNumber"
-          v-model="phone"
+          v-model="form.phoneNumber"
           title="Telefoonnummer"
           type="tel"
           name="phoneNumber"
         />
         <form-input-text
           id="companyName"
-          v-model="companyName"
+          v-model="form.companyName"
           title="Bedrijfsnaam"
           name="companyName"
         />
         <form-input-text
           id="totalAttendees"
-          v-model="totalAttendees"
+          v-model="form.totalAttendees"
           title="Aantal mensen"
           name="totalAttendees"
         />
         <form-input-text
           id="date"
-          v-model="date"
+          v-model="form.date"
           title="Streefdatum"
           type="date"
           name="date"
         />
       </form-fieldset>
-    </div>
-  </form>
+    </app-form>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { required, email } from 'vuelidate/lib/validators'
+
 import FormFieldset from '~/components/Forms/FormFieldset.vue'
 import FormInputText from '~/components/Forms/FormInputText.vue'
+import AppForm from '~/components/Forms/AppForm.vue'
 
 export default {
   components: {
     FormFieldset,
-    FormInputText
+    FormInputText,
+    AppForm
   },
   data() {
     return {
-      email: '',
-      name: '',
-      phoneNumber: '',
-      companyName: '',
-      totalAttendees: '',
-      date: ''
+      submitted: false,
+
+      form: {
+        email: 'michil@test.com',
+        name: '12312',
+        phoneNumber: '',
+        companyName: '',
+        totalAttendees: '',
+        date: ''
+      }
+    }
+  },
+  validations: {
+    form: {
+      name: {
+        required
+      },
+      email: {
+        required,
+        email
+      }
+    }
+  },
+  computed: {
+    errorMessageName() {
+      if (this.$v.form.name.$anyError) {
+        if (!this.$v.form.name.required) {
+          return this.$t('form.errors.general.required', {
+            field: this.$t('form.fields.name').toLowerCase()
+          })
+        }
+      }
+      return null
+    },
+    errorMessageEmail() {
+      if (this.$v.form.email.$anyError) {
+        if (!this.$v.form.email.required) {
+          return this.$t('form.errors.general.required', {
+            field: this.$t('form.fields.email').toLowerCase()
+          })
+        }
+
+        if (!this.$v.form.email.email) {
+          return this.$t('form.errors.fields.email.invalidEmail')
+        }
+      }
+      return null
+    }
+  },
+  methods: {
+    encodeFormData(data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&')
+    },
+    validate() {
+      this.$v.$touch()
+      return !this.$v.$invalid
+    },
+    async submitForm() {
+      if (this.validate()) {
+        await axios.post('/', this.encodeFormData(this.form), {
+          header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        })
+        this.submitted = true
+      }
     }
   }
 }
---
+++ components/Forms/AppForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form
+    data-netlify="true"
+    action
+    method="post"
+    :name="name"
+    @submit.prevent="$emit('submit')"
+  >
+    <slot />
+    <app-button type="submit">{{ buttonTitle }}</app-button>
+  </form>
+</template>
+
+<script>
+import AppButton from '@/components/Shared/AppButton.vue'
+
+export default {
+  components: {
+    AppButton
+  },
+  props: {
+    buttonTitle: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
