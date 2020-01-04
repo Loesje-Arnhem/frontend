ac087270f144ff5cc3a5feadffd788972adb6717
Michiel Koning <mail@michielkoning.nl>
2020-01-04 14:29:53 +0100

    randomize styling of tiles

--- components/Posters/RelatedPosters/RelatedPostersListItem.vue
+++ components/Posters/RelatedPosters/RelatedPostersListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li :class="$style['list-item']">
+  <li v-show="transform" :class="$style['list-item']" :style="{ transform }">
     <poster-tile :poster="poster.node" />
   </li>
 </template>
@@ -15,6 +15,36 @@ export default {
     poster: {
       type: Object,
       default: () => {}
+    }
+  },
+  data() {
+    return {
+      transform: null
+    }
+  },
+  mounted() {
+    if (!this.transform) this.generateStyle()
+  },
+  methods: {
+    generateStyle() {
+      this.transform = `${this.randomizeRotate()} ${this.randomizeScale()} ${this.randomizeTranslate()}`
+    },
+    randomize() {
+      return Math.floor(Math.random() * 8 - 4)
+    },
+    randomizeTranslate() {
+      const randomize = this.randomize() / 4
+      const translateX = randomize
+      const translateY = randomize
+      return `translate(${translateX}em, ${translateY}em)`
+    },
+    randomizeScale() {
+      const scale = (this.randomize() * 5 + 100) / 100
+      return `scale(${scale})`
+    },
+    randomizeRotate() {
+      const rotate = this.randomize()
+      return `rotate(${rotate}deg)`
     }
   }
 }
