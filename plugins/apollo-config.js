3131113e6efa60755937c161f7c383bc3c22c18e
Michiel Koning <mail@michielkoning.nl>
2020-02-04 20:13:43 +0100

    remove apollostore

--- plugins/apollo-config.js
+++ plugins/apollo-config.js
@@ -1,57 +1,3 @@
-import gql from 'graphql-tag'
-import selectedTagsQuery from '~/graphql/local/SelectedTags.gql'
-import searchTextQuery from '~/graphql/local/SearchText.gql'
-
-const typeDefs = gql`
-  type SelectedTag {
-    id: ID!
-    tagId: Int!
-  }
-  type SearchText {
-    searchText: String
-  }
-  extend type Mutation {
-    udpateSearch(search: String): SearchText
-    addToSelectedTags(id: ID!, tagId: Int!): SelectedTag
-    removeFromSelectedTags(id: ID!): Boolean
-  }
-`
-
-const resolvers = {
-  Mutation: {
-    udpateSearchText: (_, { searchText }, { cache }) => {
-      const newItem = {
-        __typename: 'SearchText',
-        searchText
-      }
-      const data = {
-        searchText: newItem
-      }
-      cache.writeQuery({ query: searchTextQuery, data })
-      return newItem
-    },
-    addToSelectedTags: (_, { id, tagId }, { cache }) => {
-      const data = cache.readQuery({ query: selectedTagsQuery })
-      const newItem = {
-        __typename: 'Tag',
-        id,
-        tagId
-      }
-      data.selectedTags.push(newItem)
-      cache.writeQuery({ query: selectedTagsQuery, data })
-      return newItem
-    },
-
-    removeFromSelectedTags: (_, { id }, { cache }) => {
-      const data = cache.readQuery({ query: selectedTagsQuery })
-      const currentItem = data.selectedTags.find(item => item.id === id)
-      data.selectedTags.splice(data.selectedTags.indexOf(currentItem), 1)
-      cache.writeQuery({ query: selectedTagsQuery, data })
-      return true
-    }
-  }
-}
-
 export default function(context) {
   return {
     httpEndpoint: 'https://api.loesje.michielkoning.nl/graphql',
@@ -61,18 +7,6 @@ export default function(context) {
     wsEndpoint: null,
     tokenName: 'apollo-token',
     persisting: false,
-    websocketsOnly: false,
-    resolvers,
-    typeDefs,
-    onCacheInit: cache => {
-      const data = {
-        selectedTags: [],
-        searchText: {
-          __typename: 'SearchText'
-        }
-      }
-
-      cache.writeData({ data })
-    }
+    websocketsOnly: false
   }
 }
