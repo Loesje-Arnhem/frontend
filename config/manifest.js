038d1ec4c973d735ecbf5519aefae9369c909af9
Michiel Koning <michiel.koning@valtech.com>
2020-06-16 16:32:53 +0200

    shortcuts

--- config/manifest.js
+++ config/manifest.js
@@ -3,11 +3,25 @@ import { themeColor, baseUrl, title } from '../data/siteDetails'
 export default {
   manifest: {
     name: title,
-    short_name: 'Adler',
+    short_name: title,
     description: 'Blijf plakken',
     lang: 'nl',
     start_url: baseUrl,
     theme_color: themeColor,
-    background_color: '#ffffff',
+    background_color: themeColor,
+    shortcuts: [
+      {
+        name: 'Posters',
+        short_name: 'Posters',
+        url: '/posters',
+        icons: [{ src: '/icon.png', sizes: '512x512' }],
+      },
+      {
+        name: 'Nieuws',
+        short_name: 'Nieuws',
+        url: '/over-mij/nieuws',
+        icons: [{ src: '/icon.png', sizes: '512x512' }],
+      },
+    ],
   },
 }
