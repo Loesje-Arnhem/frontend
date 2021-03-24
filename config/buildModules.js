8199c348e79bfce6a95ab9201d24fd655d652bef
Michiel Koning <mail@michielkoning.nl>
2021-03-24 21:35:03 +0100

    clean up packages

--- config/buildModules.js
+++ config/buildModules.js
@@ -1,7 +1,8 @@
-/*
- ** Nuxt.js modules
- */
+// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 export default [
+  // https://go.nuxtjs.dev/eslint
+  '@nuxtjs/eslint-module',
+  // https://go.nuxtjs.dev/stylelint
   '@nuxtjs/stylelint-module',
   '@nuxtjs/composition-api',
   '@nuxtjs/google-fonts',
--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,6 @@
  ** Plugins to load before mounting the App
  */
 export default [
-  '~/plugins/vue-youtube',
   '~/plugins/axios',
   '~/plugins/apollo',
   { src: '~/plugins/houdini', mode: 'client' },
