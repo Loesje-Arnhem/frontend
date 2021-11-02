5062917eb612f64bf50a509ba02d0d0d389c40a7
Michiel Koning <mail@michielkoning.nl>
2021-11-02 20:54:43 +0100

    splash

--- config/pwa.js
+++ config/pwa.js
@@ -16,6 +16,20 @@ export default {
     name: title,
     orientation: 'portrait-primary',
     short_name: title,
+    icons: [
+      {
+        src: '/icons/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icons/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
   },
   meta: {
     appleStatusBarStyle: 'black-translucent',
