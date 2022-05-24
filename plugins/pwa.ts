869b96ddae82603ad29b60a118b0d79699e9a2d2
Michiel Koning <mail@michielkoning.nl>
2022-05-24 20:06:03 +0200

    pwa

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -1,10 +1,10 @@
 // import { title, baseUrl } from '~/data/siteDetails'
 
-const urlParams = new URLSearchParams(window.location.search)
-const standalone = urlParams.get('standalone')
-if (standalone === 'true') {
-  document.documentElement.classList.add('standalone')
-}
+// const urlParams = new URLSearchParams(window.location.search)
+// const standalone = urlParams.get('standalone')
+// if (standalone === 'true') {
+//   document.documentElement.classList.add('standalone')
+// }
 
 // if ('registerProtocolHandler' in navigator) {
 //   navigator.registerProtocolHandler(
