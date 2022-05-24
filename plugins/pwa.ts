beaaea19eaf6f5db7cf6f443e7f034b83091b94a
Michiel Koning <mail@michielkoning.nl>
2022-05-24 16:20:35 +0200

    pwa

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -3,3 +3,7 @@ const standalone = urlParams.get('standalone')
 if (standalone === 'true') {
   document.documentElement.classList.add('standalone')
 }
+
+if ('registerProtocolHandler' in navigator) {
+  navigator.registerProtocolHandler('web+loesje', 'posters/%s')
+}
