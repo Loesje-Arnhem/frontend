a25098c936186deec6a5d910aa35d23dce3a275f
Michiel Koning <mail@michielkoning.nl>
2022-05-26 15:15:57 +0200

    pwa

--- graphql/Posters/Fragments/PosterRelatedProducts.ts
+++ graphql/Posters/Fragments/PosterRelatedProducts.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client/core'
 import product from './../../Products/Fragments/ProductListItem'
 
 export default gql`
-  fragment relatedProducts on Poster_Relatedproductsgroup {
+  fragment posterRelatedProducts on Poster_Relatedproductsgroup {
     title: relatedProductsTitle
     products: relatedProductsProducts {
       product {
--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -1,12 +1,20 @@
-// import { title, baseUrl } from '~/data/siteDetails'
-
 import { Context } from '@nuxt/types'
 
-// const urlParams = new URLSearchParams(window.location.search)
-// const standalone = urlParams.get('standalone')
-// if (standalone === 'true') {
-//   document.documentElement.classList.add('standalone')
-// }
+export default ({ route, app, redirect }: Context) => {
+  const standalone = route.query.standalone
+
+  if (standalone !== 'true') {
+    return
+  }
+
+  const postersPath = app.localePath({ name: 'posters' })
+  if (route.matched[0].path !== postersPath) {
+    return redirect('posters')
+  }
+}
+
+// web+tea://chai
+// web+loesje://hoeveel-warmtepompen-kan-je-eigenlijk-kopen-voor-de-prijs-van-een-kerncentrale
 
 // if ('registerProtocolHandler' in navigator) {
 //   navigator.registerProtocolHandler(
@@ -16,16 +24,3 @@ import { Context } from '@nuxt/types'
 //     title,
 //   )
 // }
-
-export default ({ route, app, redirect }: Context) => {
-  if (!window.matchMedia('(display-mode: standalone)').matches) {
-    return
-  }
-  const postersPath = app.localePath({ name: 'posters' })
-  if (route.matched[0].path !== postersPath) {
-    return redirect('posters')
-  }
-}
-
-// web+tea://chai
-// web+loesje://hoeveel-warmtepompen-kan-je-eigenlijk-kopen-voor-de-prijs-van-een-kerncentrale
