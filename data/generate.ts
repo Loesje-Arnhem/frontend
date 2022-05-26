c58c4bbde817c4ac812ac074464c1b81c4e51d34
Michiel Koning <mail@michielkoning.nl>
2022-05-26 12:37:59 +0200

    tweak generate

--- data/generate.ts
+++ data/generate.ts
@@ -1,5 +1,5 @@
-export const FETCH_TIMEOUT = 1000
-export const TOTAL_PAGES = 25
-export const TOTAL_POSTS = 25
+export const FETCH_TIMEOUT = 2000
+export const TOTAL_PAGES = 10
+export const TOTAL_POSTS = 10
 export const TOTAL_POSTERS = 25
 export const TOTAL_PRODUCT_CATEGORIES = 10
