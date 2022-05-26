230e1e9b2813c8459c6748b4009334903a90bf0c
Michiel Koning <mail@michielkoning.nl>
2022-05-26 17:10:54 +0200

    details

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -2,15 +2,17 @@ import { Context } from '@nuxt/types'
 
 export default ({ route, app, redirect }: Context) => {
   window.addEventListener('DOMContentLoaded', () => {
-    window
-      .matchMedia('(display-mode: standalone)')
-      .addEventListener('change', (event) => {
+    window.matchMedia('(display-mode: standalone)').addEventListener(
+      'change',
+      (event) => {
         if (event.matches) {
           const postersPath = app.localePath({ name: 'posters' })
           if (route.matched[0].path !== postersPath) {
             return redirect(postersPath)
           }
         }
-      })
+      },
+      { once: true },
+    )
   })
 }
