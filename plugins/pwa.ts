d811b0777541388d1e10b089833bc1821cb5bf91
Michiel Koning <mail@michielkoning.nl>
2022-06-10 14:39:27 +0200

    pwa

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -1,6 +1,9 @@
 import { Context } from '@nuxt/types'
 
 export default ({ route, app, redirect }: Context) => {
+  if (route.query.standalone === 'true') {
+    document.documentElement.classList.add('standalone')
+  }
   window.addEventListener('DOMContentLoaded', () => {
     window.matchMedia('(display-mode: standalone)').addEventListener(
       'change',
