d4134c8fd8be4c7a6cd9810168110a68ad16367f
Michiel Koning <mail@michielkoning.nl>
2022-06-10 15:05:03 +0200

    setBodyClass

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -1,14 +1,20 @@
 import { Context } from '@nuxt/types'
 
+const setBodyClass = () => {
+  document.documentElement.classList.add('standalone')
+}
+
 export default ({ route, app, redirect }: Context) => {
   if (route.query.standalone === 'true') {
-    document.documentElement.classList.add('standalone')
+    setBodyClass()
   }
   window.addEventListener('DOMContentLoaded', () => {
     window.matchMedia('(display-mode: standalone)').addEventListener(
       'change',
       (event) => {
         if (event.matches) {
+          setBodyClass()
+
           const postersPath = app.localePath({ name: 'posters' })
           if (route.matched[0].path !== postersPath) {
             return redirect({
