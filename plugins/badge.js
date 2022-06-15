90665fc9dce8a67621df5bd8af490a09fef1ad34
Michiel Koning <mail@michielkoning.nl>
2022-06-15 21:24:44 +0200

    badge

--- plugins/badge.js
+++ plugins/badge.js
@@ -8,22 +8,30 @@
 const TOTAL_POSTERS_KEY = 'total'
 
 const fetchTotalPosts = async () => {
-  const storedTotal = window.localStorage.getItem(TOTAL_POSTERS_KEY)
-
   const response = await fetch(`/total.json`)
   if (!response.ok) {
     return
   }
   const { total } = await response.json()
+  return Number(total)
+}
 
-  if (total === storedTotal) {
+const setNewBadgeCount = async () => {
+  const total = await fetchTotalPosts()
+  const storedTotal = window.localStorage.getItem(TOTAL_POSTERS_KEY)
+
+  if (total === Number(storedTotal)) {
     return
   }
-  const newTotal = Number(total) - Number(storedTotal)
-
-  window.localStorage.setItem(TOTAL_POSTERS_KEY, total)
+  const newTotal = total - Number(storedTotal)
 
   navigator.setAppBadge(newTotal)
+}
+
+const reset = async () => {
+  navigator.clearAppBadge()
+  const total = await fetchTotalPosts()
+  window.localStorage.setItem(TOTAL_POSTERS_KEY, total)
 }
 
 export default async () => {
@@ -36,6 +44,7 @@ export default async () => {
   if (!'serviceWorker' in navigator) {
     return
   }
+
   const registration = await navigator.serviceWorker.ready
 
   // Check if periodicSync is supported
@@ -44,20 +53,27 @@ export default async () => {
     return
   }
 
-  // Request permission
-  const status = await navigator.permissions.query({
-    name: 'periodic-background-sync',
-  })
-  if (status.state !== 'granted') {
-    return
+  const registerPeriodicSync = async () => {
+    // Request permission
+
+    const status = await navigator.permissions.query({
+      name: 'periodic-background-sync',
+    })
+    if (status.state !== 'granted') {
+      return
+    }
+    // Register new sync every 24 hours
+    await registration.periodicSync.register(TOTAL_POSTERS_KEY, {
+      minInterval: 24 * 60 * 60 * 1000, // 1 day
+    })
   }
-  // Register new sync every 24 hours
-  await registration.periodicSync.register(TOTAL_POSTERS_KEY, {
-    minInterval: 24 * 60 * 60 * 1000, // 1 day
-  })
 
   const tags = await registration.periodicSync.getTags()
   if (tags.includes(TOTAL_POSTERS_KEY)) {
-    fetchTotalPosts()
+    await setNewBadgeCount()
   }
+
+  registerPeriodicSync()
+
+  reset()
 }
