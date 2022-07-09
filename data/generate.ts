881e6bac9e62fe8f348e8ee4c27bde4cf00d3763
Michiel Koning <mail@michielkoning.nl>
2022-07-09 22:58:06 +0200

    timeout

--- data/generate.ts
+++ data/generate.ts
@@ -1,5 +1,5 @@
 export const FETCH_TIMEOUT = 1000
-export const TOTAL_PAGES = 20
-export const TOTAL_POSTS = 20
-export const TOTAL_POSTERS = 20
+export const TOTAL_PAGES = 50
+export const TOTAL_POSTS = 50
+export const TOTAL_POSTERS = 50
 export const TOTAL_PRODUCT_CATEGORIES = 10
