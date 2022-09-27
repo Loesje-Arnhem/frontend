7c2a72aef504285cee2b19292003c6f8ce1a7e27
Michiel Koning <mail@michielkoning.nl>
2022-09-27 21:17:40 +0200

    pwa

--- composables/useFavorites.ts
+++ composables/useFavorites.ts
@@ -1,56 +1,9 @@
 import { ref } from '@nuxtjs/composition-api'
+// our fake endpoint to store data
+const SHARED_DATA_ENDPOINT = '/token'
 
-const favorites = ref([] as number[])
 const FAVORITES_KEY = 'favorites'
-
-// see: https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim
-self.addEventListener('activate', (event) => {
-  /* @ts-ignore */
-  event.waitUntil(clients.claim())
-  console.log('aaaa')
-})
-
-self.addEventListener('fetch', (event) => {
-  const {
-    /* @ts-ignore */
-    request,
-    /* @ts-ignore */
-    request: { url, method },
-  } = event
-  if (url.match(FAVORITES_KEY)) {
-    if (method === 'POST') {
-      /* @ts-ignore */
-      request.json().then((body) => {
-        caches.open('favorites').then(function (cache) {
-          cache.put('favorites', new Response(JSON.stringify([1, 2, 3])))
-        })
-      })
-      return new Response('{}')
-    } else {
-      /* @ts-ignore */
-      event.respondWith(
-        caches.open('favorites').then(function (cache) {
-          return (
-            cache.match('favorites').then(function (response) {
-              return response || new Response('{}')
-            }) || new Response('{}')
-          )
-        }),
-      )
-    }
-  } else {
-    return event
-  }
-})
-await fetch(FAVORITES_KEY, {
-  method: 'POST',
-  body: JSON.stringify({ favorites: [1, 2, 4] }),
-})
-const response = await fetch(FAVORITES_KEY)
-console.log(response)
-const data = await response.json()
-const storedFavorites = data
-console.log(storedFavorites)
+const favorites = ref([] as number[])
 
 export default () => {
   const add = (posterId: number) => {
@@ -63,46 +16,35 @@ export default () => {
     updateStorage()
   }
 
-  const updateStorage = async () => {
+  const updateStorage = () => {
     if (!process.client) {
       return
     }
 
-    try {
-      const cache = await caches.open(FAVORITES_KEY)
-      await cache.put(
-        FAVORITES_KEY,
-        new Response(JSON.stringify({ favorites: [1, 2, 3] })),
-      )
-    } catch (error) {
-      console.log(error)
-    }
-
-    // window.localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites.value))
-    // try {
-    //   await fetch(FAVORITES_KEY, {
-    //     method: 'POST',
-    //     body: JSON.stringify({ favorites: [1, 2, 4] }),
-    //   })
-    // } catch {}
+    window.localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites.value))
+    fetch(SHARED_DATA_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify({ token: 'sampletoken' }),
+    }).then(() => {
+      console.log('saved to cache')
+    })
   }
 
-  const getFromStorage = async () => {
+  const getFromStorage = () => {
     if (!process.client) {
       return
     }
+    const storedFavorites = window.localStorage.getItem(FAVORITES_KEY)
 
-    try {
-      const cache = await caches.open(FAVORITES_KEY)
-      const response = await cache.match(FAVORITES_KEY)
-      const storedFavorites = response
-      // if (!storedFavorites) {
-      //   return
-      // }
-      console.log(storedFavorites)
-    } catch (error) {
-      console.log(error)
+    if (!storedFavorites) {
+      return
     }
+    favorites.value = JSON.parse(storedFavorites)
+    fetch(SHARED_DATA_ENDPOINT)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Got', data, 'from cache')
+      })
   }
 
   return {
