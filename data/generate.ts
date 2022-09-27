f9a8c859b920f3ff5e750c57360a15f984d15c72
MIchiel Koning <michiel.koning@valtech.com>
2022-09-27 12:10:46 +0200

    posters

--- data/generate.ts
+++ data/generate.ts
@@ -1,5 +1,5 @@
 export const FETCH_TIMEOUT = 1000
 export const TOTAL_PAGES = 20
 export const TOTAL_POSTS = 20
-export const TOTAL_POSTERS = 20
+export const TOTAL_POSTERS = 1
 export const TOTAL_PRODUCT_CATEGORIES = 10
