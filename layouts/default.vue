60209d78fe069fa8853fbc68509a6446d606393c
MIchiel Koning <michiel.koning@valtech.com>
2022-09-29 15:48:37 +0200

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,18 +35,19 @@ export default defineComponent({
       getFromStorage()
 
       if (!process.client) {
+        return
+      }
+      // @ts-ignore
+      const workbox = await window.$workbox
+      if (workbox) {
         // @ts-ignore
-        const workbox = await window.$workbox
-        if (workbox) {
-          // @ts-ignore
-          workbox.addEventListener('installed', (event) => {
-            // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
-            if (event.isUpdate) {
-              // whatever logic you want to use to notify the user that they need to refresh the page.
-              hasUpdate.value = false
-            }
-          })
-        }
+        workbox.addEventListener('installed', (event) => {
+          // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
+          if (event.isUpdate) {
+            // whatever logic you want to use to notify the user that they need to refresh the page.
+            hasUpdate.value = false
+          }
+        })
       }
     })
     return {
