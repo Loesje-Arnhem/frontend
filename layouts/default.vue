3130344d18261f9c0637ea8d9bebd98450ebd0bb
MIchiel Koning <michiel.koning@valtech.com>
2022-10-03 10:06:50 +0200

    remove event2

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,6 @@
     <window-controls-overlay />
     <header-top class="page-header-top sa-hidden" />
     <the-header class="page-header sa-hidden" />
-    {{ event2 }}
 
     <div>
       hasUpdate={{ hasUpdate }}
@@ -30,8 +29,6 @@ export default defineComponent({
 
     const hasUpdate = ref(false)
 
-    const event2 = ref(null)
-
     const clickToUpdate = () => {
       hasUpdate.value = false
       window.location.reload()
@@ -48,7 +45,6 @@ export default defineComponent({
       if (workbox) {
         // @ts-ignore
         workbox.addEventListener('installed', (event) => {
-          event2.value = event
           // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
           if (event.isUpdate) {
             // whatever logic you want to use to notify the user that they need to refresh the page.
@@ -58,7 +54,6 @@ export default defineComponent({
       }
     })
     return {
-      event2,
       hasUpdate,
       clickToUpdate,
     }
