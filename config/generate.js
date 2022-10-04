1b2b8feb2222a96e7bf7e9fc861bc1a53e3b9a74
Michiel Koning <mail@michielkoning.nl>
2022-10-04 22:57:18 +0200

    generate

--- config/generate.js
+++ config/generate.js
@@ -62,14 +62,16 @@ export default {
       websocketsOnly: false,
     })
 
-    const posters = await fetchPosters(client)
-    await pauseFetching()
-    const pages = await fetchPages(client)
-    await pauseFetching()
-    const productCategories = await fetchProductCategories(client)
-    await pauseFetching()
-    const posts = await fetchPosts(client)
+    return []
 
-    return [...pages, ...posts, ...posters, ...productCategories]
+    // const posters = await fetchPosters(client)
+    // await pauseFetching()
+    // const pages = await fetchPages(client)
+    // await pauseFetching()
+    // const productCategories = await fetchProductCategories(client)
+    // await pauseFetching()
+    // const posts = await fetchPosts(client)
+
+    // return [...pages, ...posts, ...posters, ...productCategories]
   },
 }
