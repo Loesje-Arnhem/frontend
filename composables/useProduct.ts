c86e70e351bc5e3d213afb4d055a0c9e34a0943f
MIchiel Koning <michiel.koning@valtech.com>
2022-10-05 10:12:52 +0200

    npm

--- composables/useProduct.ts
+++ composables/useProduct.ts
@@ -1,8 +1,6 @@
 import { useQuery } from '@vue/apollo-composable'
 import { computed } from '@nuxtjs/composition-api'
 import ProductsQuery from '~/graphql/Products/Products.gql'
-import ProductQuery from '~/graphql/Products/Product.gql'
-import useFetch from '~/composables/useFetch'
 
 export default (where = {}, size = 99) => {
   const { result, error, loading } = useQuery(ProductsQuery, {
