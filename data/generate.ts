473825ba0e0e3a7b1847ab3512f36c3bc925efc9
Michiel Koning <mail@michielkoning.nl>
2022-10-14 11:23:47 +0200

    fetch more

--- data/generate.ts
+++ data/generate.ts
@@ -1,5 +1,5 @@
 export const FETCH_TIMEOUT = 1000
-export const TOTAL_PAGES = 20
-export const TOTAL_POSTS = 20
-export const TOTAL_POSTERS = 15
-export const TOTAL_PRODUCT_CATEGORIES = 10
+export const TOTAL_PAGES = 40
+export const TOTAL_POSTS = 40
+export const TOTAL_POSTERS = 40
+export const TOTAL_PRODUCT_CATEGORIES = 20
