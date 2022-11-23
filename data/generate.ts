68ebdbe1b3b2c2358b4581a834bb44c03ce82587
Michiel Koning <mail@michielkoning.nl>
2022-11-23 23:27:38 +0100

    TOTAL_POSTERS

--- data/generate.ts
+++ data/generate.ts
@@ -1,5 +1,5 @@
 export const FETCH_TIMEOUT = 1000
 export const TOTAL_PAGES = 20
 export const TOTAL_POSTS = 20
-export const TOTAL_POSTERS = 15
+export const TOTAL_POSTERS = 10
 export const TOTAL_PRODUCT_CATEGORIES = 10
