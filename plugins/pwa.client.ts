d661d9678582fdb3c54f106c3e4bc3b409523fc7
MIchiel Koning <michiel.koning@valtech.com>
2022-11-29 20:16:45 +0100

    owa

--- plugins/pwa.client.ts
+++ plugins/pwa.client.ts
@@ -1,31 +1,41 @@
 import { Context } from '@nuxt/types'
 import { mediaQueryStandAlone } from '~/utils/media-queries'
 
-const setBodyClass = () => {
-  document.documentElement.classList.add('standalone')
-}
-
-const redirectToPostersPageOnStandalone = ({
-  route,
-  app,
-  redirect,
-}: Context) => {
-  const postersPath = app.localePath({ name: 'posters' })
-  if (route.matched[0].path !== postersPath) {
-    return redirect({
-      path: postersPath,
-      query: {
-        standalone: 'true',
-      },
-    })
-  }
-}
-
 export default (context: Context) => {
+  const { route, app, redirect } = context
+
+  const postersPath = app.localePath({ name: 'posters' })
+
+  const setBodyClass = () => {
+    document.documentElement.classList.add('standalone')
+  }
+
+  const redirectToPostersPageOnStandalone = () => {
+    if (route.matched[0].path !== postersPath) {
+      return redirect({
+        path: postersPath,
+        query: {
+          standalone: 'true',
+        },
+      })
+    }
+  }
+
   if (window.matchMedia(mediaQueryStandAlone).matches) {
     setBodyClass()
-    redirectToPostersPageOnStandalone(context)
+    redirectToPostersPageOnStandalone()
   }
+
+  const redirectIfProtocol = () => {
+    const protocol = 'web+loesje'
+    if (route.fullPath.includes(encodeURIComponent(protocol))) {
+      const decodedUrl = decodeURIComponent(route.fullPath)
+      const url = decodedUrl.replace(`${protocol}://`, '/')
+      window.location.href = url
+    }
+  }
+
+  redirectIfProtocol()
 
   window.addEventListener('DOMContentLoaded', () => {
     window
@@ -33,7 +43,7 @@ export default (context: Context) => {
       .addEventListener('change', (event) => {
         if (event.matches) {
           setBodyClass()
-          redirectToPostersPageOnStandalone(context)
+          redirectToPostersPageOnStandalone()
         }
       })
   })
