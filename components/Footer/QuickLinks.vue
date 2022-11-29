1e922c9e08a9be142b397e1e5866f9185c2a1c20
MIchiel Koning <michiel.koning@valtech.com>
2022-11-29 13:44:08 +0100

    CompanyAddress.vue

--- components/Footer/QuickLinks.vue
+++ components/Footer/QuickLinks.vue
@@ -3,30 +3,33 @@
     <h2 id="quicklinks-title">Handige links</h2>
     <ul class="list">
       <li>
-        <nuxt-link to="/workshops/">
-          <app-icon icon="chevron-right" width="12" height="12" />
+        <nuxt-link to="/workshops/" class="link">
+          <app-icon icon="chevron-right" width="12" height="12" class="icon" />
           Workshops
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/doe-mee/lokale-groepen/">
-          <app-icon icon="chevron-right" width="12" height="12" />Lokale groepen
+        <nuxt-link to="/doe-mee/lokale-groepen/" class="link">
+          <app-icon icon="chevron-right" width="12" height="12" class="icon" />
+          Lokale groepen
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/over-loesje/internationaal/">
-          <app-icon icon="chevron-right" width="12" height="12" />Loesje
-          internationaal
+        <nuxt-link to="/over-loesje/internationaal/" class="link">
+          <app-icon icon="chevron-right" width="12" height="12" class="icon" />
+          Loesje internationaal
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/doe-mee">
-          <app-icon icon="chevron-right" width="12" height="12" />Doe mee
+        <nuxt-link to="/doe-mee" class="link">
+          <app-icon icon="chevron-right" width="12" height="12" class="icon" />
+          Doe mee
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/doe-mee/donateur/">
-          <app-icon icon="chevron-right" width="12" height="12" />Word donateur
+        <nuxt-link to="/doe-mee/donateur/" class="link">
+          <app-icon icon="chevron-right" width="12" height="12" class="icon" />
+          Word donateur
         </nuxt-link>
       </li>
     </ul>
@@ -36,5 +39,17 @@
 <style lang="postcss" scoped>
 .list {
   @mixin list-reset;
+
+  padding-top: 0.5em;
+}
+
+.link {
+  display: flex;
+  margin-bottom: 0.25em;
+  gap: 0.25em;
+}
+
+.icon {
+  transform: translateY(0.5em);
 }
 </style>
