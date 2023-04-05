7251f734098150bc1bfe535713bab9ebfb636211
Michiel Koning <mail@michielkoning.nl>
2023-04-05 22:28:58 +0200

    generate

--- data/routes.ts
+++ data/routes.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 const fetchByType = async (type: string) => {
   const baseUrl = 'https://shop.loesje.nl'
-  const apiUrl = `${baseUrl}/wp-json/wp/v2/${type}/?_fields[]=link&per_page=99`
+  const apiUrl = `${baseUrl}/wp-json/wp/v2/${type}/?_fields[]=link&per_page=20`
   const response = await fetch(apiUrl)
   const data = (await response.json()) as { link: string }[]
   let suffix = ''
