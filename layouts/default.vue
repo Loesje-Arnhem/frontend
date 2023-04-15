889572d580effed791261dd9f8033fb6ca767e1e
Michiel Koning <mail@michielkoning.nl>
2023-04-15 22:05:03 +0200

    mmeta

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,23 +24,23 @@ onMounted(() => {
           v-for="link in head.link"
           :key="link.id"
         >
-          <link
+          <Link
             :id="link.id"
             :rel="link.rel"
             :href="link.href"
             :hreflang="link.hreflang"
-          >
+          />
         </template>
 
         <template
           v-for="meta in head.meta"
           :key="meta.id"
         >
-          <meta
+          <Meta
             :id="meta.id"
             :property="meta.property"
             :content="meta.content"
-          >
+          />
         </template>
       </Head>
       <Body>
