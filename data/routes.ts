ef1982adc12fe7f2fe85b46804b344fab1f54fb7
MIchiel Koning <michiel.koning@valtech.com>
2023-06-24 11:35:19 +0200

    routes

--- data/routes.ts
+++ data/routes.ts
@@ -1,24 +1,48 @@
-import fetch from 'node-fetch'
+import { ofetch } from 'ofetch'
 
-const fetchByType = async (type: string) => {
+const PAGESIZE = 99
+const FETCH_TIMEOUT = 1000
+
+const pauseFetching = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, FETCH_TIMEOUT)
+  })
+}
+
+const fetchPagesByType = async (type: string) => {
+  const pages = []
+  let hasNextPage = true
+  let page = 1
   const baseUrl = 'https://shop.loesje.nl'
-  const apiUrl = `${baseUrl}/wp-json/wp/v2/${type}/?_fields[]=link&per_page=20`
-  const response = await fetch(apiUrl)
-  const data = (await response.json()) as { link: string }[]
-  let suffix = ''
-  if (type === 'posts') {
-    suffix = `/over-loesje/nieuws`
+
+  while (hasNextPage) {
+    const apiUrl = `${baseUrl}/wp-json/wp/v2/${type}/?_fields[]=link&per_page=${PAGESIZE}&page=${[page]}`
+    const data = await ofetch<{ link: string }[]>(apiUrl)
+
+    let suffix = ''
+    if (type === 'posts') {
+      suffix = `/over-loesje/nieuws`
+    }
+
+    const urls = data.map((r) => r.link.replace(baseUrl, suffix))
+    pages.push(...urls)
+    if (urls.length !== PAGESIZE) {
+      hasNextPage = false
+    }
+    page = page + 1
+    pauseFetching()
   }
-  return data.map((r) => r.link.replace(baseUrl, suffix))
+  return pages
+
 }
 
 export default async () => {
-  //const posts = await fetchByType('posts')
-  //const pages = await fetchByType('pages')
-  // const posters = await fetchByType('posters')
+  const posts = await fetchPagesByType('posts')
+  const pages = await fetchPagesByType('pages')
+  const posters = await fetchPagesByType('posters')
   return [
-    // ...posts,
-    // ...pages,
-    // ...posters
+    ...posts,
+    ...pages,
+    ...posters
   ]
 }
