fc76018c4b46d3f9022db199b60d752efc92840a
MIchiel Koning <michiel.koning@valtech.com>
2023-11-21 16:04:27 +0100

    rss

--- server/routes/rss-posters.xml.ts
+++ server/routes/rss-posters.xml.ts
@@ -11,18 +11,20 @@ const getPosterDate = (date: string) => {
 }
 
 export default defineEventHandler(async (event) => {
-  const { rssHead } = useAppConfig()
-
+  const { rssHead, title } = useAppConfig()
 
   const feed = new RSS({
     ...rssHead,
-    title: 'Loesje Posters',
+    title,
     feed_url: `https://www.loesje.nl/rss-posters.xml`,
     description: `
       Loesje's posters vind je overal. Met haar positief-kritische teksten
       wil ze de wereld beter en mooier maken. Dat moet je niet overlaten aan
       bazen, politici of ouders. Door haar posters op straat en online te
       verspreiden, geeft Loesje de wereld een zetje in de goede richting.`,
+    custom_namespaces: {
+      media: "http://search.yahoo.com/mrss/",
+    },
   })
 
   const url = getUrl({
@@ -47,25 +49,52 @@ export default defineEventHandler(async (event) => {
   }
 }[]>(url)
 
+
   data.forEach(item => {
     const link = `https://www.loesje.nl/posters/${item.slug}`
+    const images = item._embedded['wp:featuredmedia']
+
     const featuredImage = getFeaturedImage(
       item._embedded['wp:featuredmedia'],
       item.title.rendered,
     )
 
-    let description = ''
-    if (featuredImage) {
-      description = `<img src="${featuredImage.src}" srcset="${featuredImage.srcSet}" alt="${item.title.rendered}" width="${featuredImage.width}" height="${featuredImage.height}" />`
+    if (!images?.length || !featuredImage) {
+      return
     }
+
+    const image = images[0].media_details.sizes.medium_large
+
+    if (!image) {
+      return
+    }
+
+    const description = `<img src="${featuredImage.src}" srcset="${featuredImage.srcSet}" alt="${item.title.rendered}" width="${featuredImage.width}" height="${featuredImage.height}" />`
 
     feed.item({
       title: item.title.rendered,
-      description: description,
       date: getPosterDate(item.acf.date) || item.date,
       url: link,
       guid: link,
       author: 'Loesje',
+      description,
+      custom_elements: [
+        {
+          'media:content': {
+            _attr: {
+              medium: 'image',
+              href: image.source_url,
+              width: image.width,
+              height: image.height,
+              type: image.mime_type,
+              fileSize: image.filesize
+            }
+          }
+        },
+        {
+          'media:title': item.title.rendered
+        }
+      ]
     })
   });
 
