231e924026640d662cc263abc89ee88b7fb36a5e
MIchiel Koning <michiel.koning@valtech.com>
2024-02-05 10:11:07 +0100

    key

--- server/api/product-categories/product-categories.get.ts
+++ server/api/product-categories/product-categories.get.ts
@@ -8,7 +8,7 @@ const sortByOrder = (items: ResponseProductCategories) => {
 export default defineEventHandler(async () => {
   const storage = useStorage('redis')
 
-  const key = 'categories'
+  const key = 'product-categories'
 
   if (await storage.getItem(key)) {
     return await storage.getItem(key)
