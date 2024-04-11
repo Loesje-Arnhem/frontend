e372a1e339f04ab6de8438106f153369c0aaf7b1
Michiel Koning <mail@michielkoning.nl>
2024-04-11 21:34:13 +0200

    clear

--- modules/redis.ts
+++ modules/redis.ts
@@ -3,10 +3,8 @@ import { defineNuxtModule } from '@nuxt/kit'
 export default defineNuxtModule({
   setup(options, nuxt) {
     nuxt.hook('nitro:build:before', async (nitro) => {
-      console.log('hook1')
       if (process.env.NODE_ENV === 'production' && process.env.NUXT_SSR === 'true') {
-        console.log('hook2')
-        await nitro.storage.clear('redis')
+        await nitro.storage.clear()
       }
     })
   },
