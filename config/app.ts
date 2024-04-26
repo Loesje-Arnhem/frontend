38647214b314e9cc54e64387f285ca157c5de284
MIchiel Koning <michiel.koning@valtech.com>
2024-04-26 10:19:21 +0200

    remove config

--- config/app.ts
+++
@@ -1,8 +0,0 @@
-import { NuxtConfig } from '@nuxt/types'
-
-const config: NuxtConfig = {
-  app: {
-    heada: {},
-  },
-}
-export default config.css
--- config/target.ts
+++
@@ -1,6 +0,0 @@
-import { NuxtConfig } from '@nuxt/types'
-
-const config: NuxtConfig = {
-  target: 'static',
-}
-export default config.target
