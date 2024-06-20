0723e3fa1be70ae027d244902b28734c98f8259c
Michiel Koning <mail@michielkoning.nl>
2024-06-20 21:10:21 +0200

    forms

--- pages/donate.vue
+++ pages/donate.vue
@@ -53,95 +53,34 @@ const closeDialog = () => {
 
 <template>
   <div v-if="data">
-    <app-content
-      :title="data.title"
-      :content="data.content"
-      :video="data.youtubeId"
-    />
+    <app-content :title="data.title" :content="data.content" :video="data.youtubeId" />
   </div>
-  <center-wrapper size="sm">
-    <div class="form widget">
-      <h1>1. Formulier</h1>
-      <clubcollect-widget
-        force-display="true"
-        type="form-page"
-        path="nl/loesje/jaarlijkse-donatie-campagne"
-      />
-    </div>
+  <center-wrapper size="md">
     <div class="widget">
-      <h1>2. Overzichtswidget</h1>
-      <clubcollect-widget
-        type="form"
-        path="nl/loesje/jaarlijkse-donatie-campagne"
-        goal="10"
-        button-color="#000000"
-      />
+      <h1>Bendeleden</h1>
+      <iframe class="form" name="clubbase-signup-form"
+        src="https://app.clubbase.io/signup/f73cc96d-dbbd-44b7-866d-ff565dd6e864?id=09bb8665-9a85-4d0a-8f09-119029c6029e" />
     </div>
 
     <div class="widget">
-      <h1>3. In een modal</h1>
-
-      <dialog ref="dialog" class="dialog">
-        <h1>Steun Loesje</h1>
-
-        <button @click="closeDialog" class="btn-close">
-          <app-icon icon="close" />
-        </button>
-        <clubcollect-widget
-          force-display="true"
-          type="form-page"
-          path="nl/loesje/jaarlijkse-donatie-campagne"
-        />
-      </dialog>
-      <app-button @click="showDialog">Toon modal</app-button>
+      <h1>Donateurs</h1>
+      <iframe class="form" name="clubbase-signup-form"
+        src="https://app.clubbase.io/signup/f73cc96d-dbbd-44b7-866d-ff565dd6e864?id=8e12b804-83e1-46a7-84a3-f38018a8dd42" />
     </div>
+
   </center-wrapper>
 </template>
 
 <style lang="postcss" scoped>
+.form {
+  border: 0;
+  width: 100%;
+  height: 100%;
+  min-height: 3000px;
+  overflow-y: scroll;
+}
+
 .widget {
   margin-bottom: 4em;
-}
-
-.form {
-  & h1 {
-    padding-inline: calc(10px + 1.07143rem);
-  }
-}
-
-.dialog {
-  padding: 1em 0 0;
-  width: 40em;
-  max-width: calc(100% - var(--gutter) * 2);
-
-  & h1 {
-    margin-inline: calc(10px + 1.07143rem);
-  }
-
-  &::backdrop {
-    background: rgba(255, 255, 255, 0.5);
-  }
-}
-
-.btn-close {
-  position: absolute;
-  right: var(--gutter);
-  top: var(--gutter);
-  background: var(--color-white);
-  color: var(--color-black);
-  border-radius: 50%;
-  padding: 0.25em;
-  font-size: 0.75em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 3px solid var(--color-black);
-  transition: var(--animation);
-  transition-property: color, background-color;
-
-  &:hover {
-    background-color: var(--color-black);
-    color: var(--color-white);
-  }
 }
 </style>
