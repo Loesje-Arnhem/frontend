beb668353f6fdab12683188c8b91842733e12f5a
Michiel Koning <mail@michielkoning.nl>
2025-04-04 23:03:38 +0200

    pwa

--- middleware/pwa.global.ts
+++ middleware/pwa.global.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
   const headers = useRequestHeaders();
 
-  if (headers["sec-ch-ua"]?.includes("Edge")) {
+  if (headers["sec-ch-ua"]?.includes("Edge Side Panel")) {
     const isPosterPage = to.matched.some((route) => route.path === "/posters");
     if (!isPosterPage) {
       return navigateTo("/posters");
